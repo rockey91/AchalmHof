6905490d72c6cf1ae3b1d939897e855d2c3563f4
rockey91 <rockey91@gmail.com>
2019-03-12 10:03:38 +0530

    Slideshow in venue detail page implemented.

--- src/app/home/components/functionhome/venuepage/venuepage.module.ts
+++ src/app/home/components/functionhome/venuepage/venuepage.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { SlideshowModule } from 'ng-simple-slideshow';
 
 import { VenuepageRoutingModule } from './venuepage-routing.module';
 import { VenuepageComponent } from './venuepage.component';
@@ -11,7 +12,8 @@ import { VenuepageComponent } from './venuepage.component';
         CommonModule,
         VenuepageRoutingModule,
         TranslateModule,
-        NgbDropdownModule
+        NgbDropdownModule,
+        SlideshowModule
     ],
     declarations: [VenuepageComponent]
 })
