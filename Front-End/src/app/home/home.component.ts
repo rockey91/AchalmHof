5de5cbb86c3abce6d4e029bdb50490a81e0a63c0
Yogesh.Shanmukhappa <Yogesh.Shanmukhappa@sonicautomotive.com>
2019-08-28 18:49:55 +0530

    styling applied

--- Front-End/src/app/home/home.component.ts
+++ Front-End/src/app/home/home.component.ts
@@ -7,7 +7,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  imageUrlArray: (string | IImage)[] = [
+    { url: 'https://achalmhof.de/wp-content/uploads/2017/04/Homepage-Startseite-Hofladen-300x300.jpg', caption: 'Funtion Halls', href: '/home/functionhalls' },
+    { url: 'https://achalmhof.de/wp-content/uploads/2016/08/events-300x300.jpg'}
+  ];
   constructor(private router: Router) {}
 
   ngOnInit(){
