ef6b1ee3f05c73dfca03125b73d0adf02aa69cc4
satishg08 <satish.g08@gmail.com>
2020-01-05 09:11:30 +0530

    Changes

--- Back-End/UpdateVenueDetails20191230.sql
+++ Back-End/UpdateVenueDetails20191230.sql
@@ -2,7 +2,7 @@ use achalm_hof;
 
 update venues set name = 'Festscheune' where id=1;
 
-update venues set name = 'Almhütte' where id=99;
+update venues set name = 'Almhütte' where id=  99;
 
 
 update venues set description = 'Unsere große Festscheune ist mit ihren 355 qm² sehr großzügig gestaltet und kann für viele verschiedene Anlässe genutzt werden. Neben Bankettbestuhlung an Rund- so wie Tafeltischen können zudem Schulungen, Tagungen und verschiedenste Feierlichkeiten veranstaltet werden. Bei entsprechendem Wetter ist es zudem möglich, Ihre Veranstaltung wie etwa eine freie Trauung in den herrlichen Außenbereich zu verlegen. In diesem ist ein Naturteich sowie ein Spielplatz für die kleinen Gäste vorhanden. ' where id=1;
